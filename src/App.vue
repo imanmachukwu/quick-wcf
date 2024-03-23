6cf1f16cc751311ddd3e9278d1424820109521d1
Imanmachukwu Chukwuemeka <iamimanma@gmail.com>
2024-03-23 09:24:13 +0100

    add responsive message for desktop

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,32 @@
 <template>
-  <!-- <nav>
-    <router-link to="/">Home</router-link>
-  </nav> -->
-  <router-view />
+  <router-view id="main-view" v-if="isMobile" />
+  <main v-if="isDesktop">
+    <p>Please use your phone to visit this site.</p>
+  </main>
 </template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      isMobile: false,
+      isDesktop: true,
+    };
+  },
+  methods: {
+    checkDevice() {
+      if (window.innerWidth <= 675) {
+        this.isMobile = true;
+        this.isDesktop = false;
+      }
+    },
+  },
+  mounted() {
+    this.checkDevice();
+  },
+};
+</script>
 
 <style lang="scss">
 @import url("https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&family=Space+Grotesk:wght@300..700&display=swap");
@@ -37,5 +60,41 @@ p {
 
 a {
   text-decoration: none;
+}
+
+#main-view {
+  display: none;
+}
+
+main {
+  width: 100dvw;
+  height: 100dvh;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 7;
+  background: #f0f0f0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow-y: hidden;
+  p {
+    color: #000;
+    font-family: "IBM Plex Sans";
+    font-size: 2.25rem;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 101.754%;
+    letter-spacing: -0.1125rem;
+  }
+}
+
+@media only screen and (max-width: 678px) {
+  #main-view {
+    display: block;
+  }
+  main {
+    display: none;
+  }
 }
 </style>
